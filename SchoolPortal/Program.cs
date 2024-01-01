fabecc59dd5d25086715e0fa828f26b5787e2196
Omkar Ware <123940891+ifeelomii@users.noreply.github.com>
2024-01-01 18:24:32 +0530

    Add files via upload

--- SchoolPortal/Program.cs
+++ SchoolPortal/Program.cs
@@ -1,9 +1,16 @@
+using model;
+using services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+builder.Services.AddSingleton<IAdminService, AdminService >();
+builder.Services.AddSingleton<IStudentService, StudentService >();
+builder.Services.AddSingleton<ITeacherService, TeacherService >();
+// builder.Services.AddSingleton<IComplaintService, ComplaintService >();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -12,6 +19,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
